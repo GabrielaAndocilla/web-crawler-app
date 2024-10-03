18db174ff31edf0221590d992e6aaf19fc485e2a
Gariela Andocilla <gariela@Garielas-MacBook-Pro.local>
2024-10-03 10:18:15 +0200

    feat: set up react configuration for ui

---
+++ web-crawler-ui/src/App.tsx
@@ -0,0 +1,13 @@
+import { useEffect } from 'react';
+import './App.css';
+
+function App() {
+  const getPage = () => {};
+  useEffect(() => {
+    getPage();
+  }, []);
+
+  return <div className="App">staart</div>;
+}
+
+export default App;
